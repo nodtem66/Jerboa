b1c5d71d2c7520e8c555378414baa83cef189577
Jirawat Iamsamang <nodtem66@gmail.com>
2010-04-25 18:03:07 +0700

    add unit test with JSSpec

---
+++ unit.js
@@ -0,0 +1,317 @@
+var $ = {};
+
+$.onDomReady = function(callback)
+{
+	
+	if(document.addEventListener)
+	{
+		//If Browser supports the DOMContentLoaded event
+		document.addEventListener("DOMContentLoaded",callback,false);
+	} else {
+		//For IE
+		if(document.body && document.body.lastChild)
+		{
+			callback();
+		} else {
+			setTimeout(arguments.callee.caller,50);
+		}
+
+	}
+}
+// Add a new namespace to the $ library to hold all event-related code,
+// using an object literal notation to add multiple methods at once
+$.Events = {
+	// The add method allows us to assign a function to execute when an
+	// event of a specified type occurs on a specific element
+	add: function (element, eventType, callback) {
+		// Store the current value of this to use within subfunctions
+		var self = this;
+		eventType = eventType.toLowerCase();
+		if (element.addEventListener) {
+		// If the W3C event listener method is available, use that
+			element.addEventListener(eventType, function(e){
+			// Execute callback function, passing it a standardized version of
+			// the event object, e. The standardize method is defined later
+			callback(self.standardize(e));
+			}, false);
+		} else if (element.attachEvent) {
+			// Otherwise use the Internet Explorer-proprietary event handler
+			element.attachEvent("on" + eventType, function() {
+			// IE uses window.event to store the current event's properties
+			callback(self.standardize(window.event));
+			});
+		}
+},
+	// The remove method allows us to remove previously assigned code
+	// from an event
+	remove: function (element, eventType, callback) {
+		eventType = eventType.toLowerCase();
+		if (element.removeEventListener) {
+			// If the W3C-specified	method is available, use that
+			element.removeEventListener(element, eventType, callback);
+		} else if (element.detachEvent) {
+			// Otherwise, use the Internet Explorer-specific method
+			element.detachEvent("on" + eventType, callback);
+		}
+},
+	// The standardize method produces a unified set of event
+	// properties, regardless of the browser
+	standardize: function(event) {
+	// These two methods, defined later, return the current position of the
+	// mouse pointer, relative to the document as a whole, and relative to the
+	// element the event occurred within
+	var page = this.getMousePositionRelativeToDocument(event);
+	var offset = this.getMousePositionOffset(event);
+	
+	// Let's stop events from firing on element nodes above the current
+	if (event.stopPropagation) {
+		event.stopPropagation();
+	} else {
+		event.cancelBubble = true;
+	}
+	// We return an object literal containing seven properties and one method
+	return {
+		
+		// The target is the element the event occurred on
+		target: this.getTarget(event),
+		
+		// The relatedTarget is the element the event was listening for,
+		// which can be different from the target if the event occurred on an
+		// element located within the relatedTarget element in the DOM
+		relatedTarget: this.getRelatedTarget(event),
+		
+		// If the event was a keyboard-related one, key returns the character
+		event: event,
+		
+		// Return the x and y coordinates of the mouse pointer,
+		// relative to the document
+		pageX: page.x,
+		pageY: page.y,
+		
+		// Return the x and y coordinates of the mouse pointer,
+		// relative to the element the current event occurred on
+		offsetX: offset.x,
+		offsetY: offset.y,
+		
+		// The preventDefault method stops the default event of the element
+		// we're acting upon from occurring. If we were listening for click
+		// events on a hyperlink, for example, this method would stop the
+		// link from being followed
+		preventDefault: function() {
+			if (event.preventDefault) {
+			event.preventDefault(); // W3C method
+			} else {
+			event.returnValue = false; // Internet Explorer method
+			}
+		}
+	};
+},
+	// The getTarget method locates the element the event occurred on
+	getTarget: function(event) {
+		// Internet Explorer value is srcElement, W3C value is target
+		var target = event.srcElement || event.target;
+		// Fix legacy Safari bug which reports events occurring on a text
+		// node instead of an element node
+		if (target.nodeType == 3) { // 3 denotes a text node
+			target = target.parentNode; // Get parent node of text node
+		}
+		// Return the element node the event occurred on
+		return target;
+},
+	// The getMousePositionRelativeToDocument method returns the current
+	// mouse pointer position relative to the top left edge of the current page
+	getMousePositionRelativeToDocument: function(event) {
+		var x = 0, y = 0;
+		if (event.pageX) {
+			// pageX gets coordinates of pointer from left of entire document
+			x = event.pageX;
+			y = event.pageY;
+		} else if (event.clientX) {
+			// clientX gets coordinates from left of current viewable area
+			// so we have to add the distance the page has scrolled onto this value
+			x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		// Return an object literal containing the x and y mouse coordinates
+		return {
+			x: x,
+			y: y
+		}
+},
+	// The getMousePositionOffset method returns the distance of the mouse
+	// pointer from the top left of the element the event occurred on
+	getMousePositionOffset: function(event) {
+		var x = 0, y = 0;
+		if (event.layerX) {
+			x = event.layerX;
+			y = event.layerY;
+		} else if (event.offsetX) {
+			// Internet Explorer-proprietary
+			x = event.offsetX;
+			y = event.offsetY;
+		}
+		// Returns an object literal containing the x and y coordinates of the
+		// mouse relative to the element the event fired on
+		return {
+			x: x,
+			y: y
+		}
+},
+	// The getRelatedTarget method returns the element node the event was set up to
+	// fire on, which can be different from the element the event actually fired on
+	getRelatedTarget: function(event) {
+		var relatedTarget = event.relatedTarget;
+		if (event.type == "mouseover") {
+			// With mouseover events, relatedTarget is not set by default
+			relatedTarget = event.fromElement;
+		} else if (event.type == "mouseout") {
+			// With mouseout events, relatedTarget is not set by default
+			relatedTarget = event.toElement;
+		}
+		return relatedTarget;
+}
+};
+
+$.CSS = {
+	
+	// The getArrayOfClassNames method is a utility method which returns an
+	// array of all the CSS class names assigned to a particular element.
+	// Multiple class names are separated by a space character
+	getArrayOfClassNames: function(element) {
+		var classNames = [];
+		if (element.className) {
+			// If the element has a CSS class specified, create an array
+			classNames = element.className.split(' ');
+		}
+		return classNames;
+},
+		// The addClass method adds a new CSS class of a given name to a
+		// particular element
+	addClass: function(element, className) {
+		// Get a list of the current CSS class names applied to the element
+		var classNames = this.getArrayOfClassNames(element);
+		// Add the new class name to the list
+		classNames.push(className);
+		// Convert the list in space-separated string and assign to the element
+		element.className = classNames.join(' ');
+},
+	// The removeClass method removes a given CSS class name from
+	// a given element
+	removeClass: function(element, className) {
+		var classNames = this.getArrayOfClassNames(element);
+		// Create a new array for storing all the final CSS class names in
+		var resultingClassNames = [];
+		for (var index = 0; index < classNames.length; index++) {
+			// Loop through every class name in the list
+			if (className != classNames[index]) {
+				// Add the class name to the new list if it isn't the one specified
+				resultingClassNames.push(classNames[index]);
+			}
+		}
+		
+		// Convert the new list into a space-separated string and assign it
+		element.className = resultingClassNames.join(" ");
+},
+	// The hasClass method returns true if a given class name exists on a
+	// specific element, false otherwise
+	hasClass: function(element, className) {
+		// Assume by default that the class name is not applied to the element
+		var isClassNamePresent = false;
+		var classNames = this.getArrayOfClassNames(element);
+		for (var index = 0; index < classNames.length; index++) {
+			// Loop through each CSS class name applied to this element
+			if (className == classNames[index]) {
+				// If the specific class name is found, set the return value to true
+				isClassNamePresent = true;
+			}
+		}
+		// Return true or false, depending on if the specified class name was found
+		return isClassNamePresent;
+},
+	// The getPosition method returns the x and y coordinates of the top-left
+	// position of a page element within the current page, along with the
+	// current width and height of that element
+	getPosition: function(element) {
+		var x = 0, y = 0;
+		var elementBackup = element;
+		if (element.offsetParent) {
+			// The offsetLeft and offsetTop properties get the position of the
+			// element with respect to its parent node. To get the position with
+			// respect to the page itself, we need to go up the tree, adding the
+			// offsets together each time until we reach the node at the top of
+			// the document, by which point, we'll have coordinates for the
+			// position of the element in the page
+			do {
+				x += element.offsetLeft;
+				y += element.offsetTop;
+				// Deliberately using = to force the loop to execute on the next
+				// parent node in the page hierarchy
+			} while (element = element.offsetParent)
+		}
+		
+		// Return an object literal with the x and y coordinates of the element,
+		// along with the actual width and height of the element
+		return {
+			x: x,
+			y: y,
+			height: elementBackup.offsetHeight,
+			width: elementBackup.offsetWidth
+		}
+		}
+};
+$.Utils = {
+	// The toCamelCase method takes a hyphenated value and converts it into
+	// a camel case equivalent, e.g., margin-left becomes marginLeft. Hyphens
+	// are removed, and each word after the first begins with a capital letter
+	toCamelCase: function(hyphenatedValue) {
+		var result = hyphenatedValue.replace(/-\D/g, function(character) {
+			return character.charAt(1).toUpperCase();
+		});
+		return result;
+},
+	// The toHyphens method performs the opposite conversion, taking a camel
+	// case string and converting it into a hyphenated one.
+	// e.g., marginLeft becomes margin-left 
+	toHyphens: function(camelCaseValue) {
+		var result = camelCaseValue.replace(/[A-Z]/g, function(character) {
+			return ('-'+ character.charAt(0).toLowerCase());
+		});
+		return result;
+}
+};
+$.Element = {
+	cache: {}
+	,set: function(element,List)
+	{
+		var listAttr = List.Attr || List.attr || {};
+		for(var key in listAttr)
+		{
+			element.setAttribute(key,listAttr[key]);
+		}
+		element.innerHTML = List.HTML || List.html || element.innerHTML
+		
+		return element;
+	}
+	,create: function(nameElement)
+	{
+		if(!this.cache[nameElement])
+			this.cache[nameElement] = document.createElement(nameElement);
+		return this.cache[nameElement];
+		
+	}
+};
+$.UI = {
+	
+};
+$.bind = function(callback,scope) {
+	var scope = scope || window;
+	var args = Array.prototype.slice.call(arguments,2);
+	return callback.apply(scope,args.concat(arguments));
+};
+$.throttle = function(callback,scope) {
+	var scope = scope || window;
+	clearTimeout(callback.tId);
+	callback.tId = setTimeout(function(){
+		callback.call(scope);
+	},100);
+};
